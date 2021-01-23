72bc753bcf38b1fe7f0be9b4ad3a611392cb722c
Harumi Terayama <hterayama@gmail.com>
2021-01-23 20:32:05 +0100

    iteration 3 completed

--- routes/authentication.js
+++ routes/authentication.js
@@ -14,7 +14,7 @@ router.post('/sign-up', (req, res, next) => {
   })
     .then((user) => {
       if (user) {
-        throw new Error('This username is already been used');
+          throw new Error('This username is already been used');
       } else {
         return bcryptjs.hash(data.password, 10);
       }
@@ -26,6 +26,7 @@ router.post('/sign-up', (req, res, next) => {
       });
     })
     .then((user) => {
+      req.session.userId = user._id;
       res.redirect('/profile');
     })
     .catch((error) => {
@@ -33,36 +34,40 @@ router.post('/sign-up', (req, res, next) => {
     });
 });
 
-router.get('/log-in', (req, res, next) =>{
-    res.render('log-in');
+router.get('/log-in', (req, res, next) => {
+  res.render('log-in');
 });
 
 router.post('/log-in', (req, res, next) => {
-    const data = req.body;
-    let user;
-    User.findOne({
-        username: data.username
+  const data = req.body;
+  let user;
+  User.findOne({
+    username: data.username
+  })
+    .then((doc) => {
+      user = doc;
+      if (user) {
+        return bcryptjs.compare(data.password, user.hashedPassword);
+      } else {
+        throw new Error('There is no user registered with that username.');
+      }
     })
-    .then(doc => {
-        user = doc;
-        if(user) {
-            return bcryptjs.compare(data.password, user.hashedPassword);
-        } else {
-            throw new Error('There is no user registered with that username.')
-        }
+    .then((result) => {
+      if (result) {
+        req.session.userId = user._id;
+        res.redirect('/profile');
+      } else {
+        throw new Error('The password does not match.');
+      }
     })
-    .then(result => {
-        if(result){
-            req.session.userId = user._id;
-            res.redirect('/profile');
-        } else {
-            throw new Error('The password does not match.');
-        }
-    })
-    .catch(error => {
-        next(error);
+    .catch((error) => {
+      next(error);
     });
+});
 
+router.post('/log-out', (req, res, next) => {
+  req.session.destroy();
+  res.redirect('/');
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,20 @@ router.get('/profile', (req, res, next) => {
   res.render('profile');
 })
 
+router.get('/main', (req, res, next) => {
+  if(req.user){
+    res.render('main');
+  } else {
+    res.redirect('/authentication/log-in');
+  }
+});
+
+router.get('/private', (req, res, next) => {
+  if(req.user){
+    res.render('private');
+  } else {
+    res.redirect('/authentication/log-in');
+  }
+});
+
 module.exports = router;
